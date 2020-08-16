19e18fd991e640db7e7166a2a3b8ce0d00d37ff2
chenwen11 <chenwen11@meituan.com>
2020-08-16 20:36:12 +0800

    Updates

---
+++ precache-manifest.dc733fd3066ce3e5277edfc09b35c283.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88a68ee6ae5c630d28f146a4251c3522",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f66067c10639e217e3c4",
+    "url": "./static/css/2.d3fceede.chunk.css"
+  },
+  {
+    "revision": "8c6109788ee3003339b5",
+    "url": "./static/css/main.f5cd0216.chunk.css"
+  },
+  {
+    "revision": "f66067c10639e217e3c4",
+    "url": "./static/js/2.895b679c.chunk.js"
+  },
+  {
+    "revision": "2e4067154986ec564d862ee2f50f3824",
+    "url": "./static/js/2.895b679c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c6109788ee3003339b5",
+    "url": "./static/js/main.6da92c11.chunk.js"
+  },
+  {
+    "revision": "62e745d1769ec61aa099",
+    "url": "./static/js/runtime-main.3dd90157.js"
+  }
+]);
